830111114f019b999da684c386498d6387463676
deependra bhattachan <deependra1993@github.com>
2017-08-14 17:44:46 +1000

    the changes has been mads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+simport { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
